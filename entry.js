553e1cccb3985825e16426b0006a8795a2135f88
Augustus Rutkoski <rutkoski.augustus@heb.com>
2016-09-02 13:05:52 -0500

    browserify working

---
+++ entry.js
@@ -0,0 +1,8 @@
+require('angular');
+require('angular-ui-router');
+require('angular-animate');
+require('angular-aria');
+require('angular-material');
+require('ng-table');
+require('ng-dialog');
+require('angular-ui-bootstrap');
